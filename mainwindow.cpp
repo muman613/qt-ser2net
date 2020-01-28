6161373ede20d2894c9c958fc57c49bb4602417a
Michael Uman <michael.uman@wunderbar.com>
2020-01-28 11:37:28 -0800

    Added functionality via lambda function callback
    * Command processing made easier with lambda callback
    * State machine implemented

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,37 +14,91 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     connect(&telnet, &QTelnet::newData, this, &MainWindow::newData);
+    connect(&telnet, &QTelnet::stateChanged, this, &MainWindow::stateChanged);
 
     telnet.connectToHost("wunderbar-ub18-dev", 2000);
 
-    QTimer::singleShot(200, this, [=]() {
-        sendCommand("version");
+#ifdef CMD_ON_TIMER
+    QTimer::singleShot(1000, this, [=]() {
+        sendCommand("version", [](QStringList & x) {
+            qDebug() << x;
+        });
     });
-
+#endif
 }
 
-void MainWindow::sendCommand(const QString &cmd)
+void MainWindow::sendCommand(const QString &cmd, CALLBACK cb)
 {
+    if (state != STATE_IDLE) {
+        qDebug() << "Cannot issue new command while old command has not responded";
+        return;
+    }
+
+    _cb = cb;
+
     command = cmd;
     response.clear();
     QByteArray ar = cmd.toLatin1();
     ar.append('\n');
 
     telnet.sendData(ar);
+    state = STATE_INCMD;
 }
 
 void MainWindow::newData(const char *buff, int len) {
     QByteArray bytes(buff, len);
+    bool bPrompt = false;
+
+    qDebug() << Q_FUNC_INFO;
+
+    if (bytes.endsWith("-> ")) {
+        qDebug() << "Found prompt!";
+        bPrompt = true;
+    }
+
+    if (state == STATE_INIT) {
+        if (bPrompt) {
+            qDebug() << "Received 1st prompt";
+            state = STATE_IDLE;
+            return;
+        }
+    }
+    if (state != STATE_INCMD) {
+        qDebug() << "Received data while not waiting for command response...";
+        return;
+    }
 
     response = bytes;
 
-    qDebug() << Q_FUNC_INFO;
+    if (_cb != nullptr) {
+        QStringList strlst = response.split('\n');
+
+        qDebug() << "Calling callback function...";
+        (*_cb)(strlst);
+
+        _cb = nullptr;
+    }
 
     ui->mainText->append(response);
     QFile out("/tmp/netlog.log");
 
     if (out.open(QFile::WriteOnly|QFile::Append)) {
         out.write(buff, len);
+    }
+
+    state = STATE_IDLE;
+}
+
+void MainWindow::stateChanged(QAbstractSocket::SocketState state)
+{
+    qDebug() << Q_FUNC_INFO << state;
+
+    if (state == QAbstractSocket::ConnectedState) {
+        QTimer::singleShot(1000, this, [=]() {
+            sendCommand("version", [](QStringList & x) {
+                qDebug() << x;
+            });
+        });
     }
 }
 
@@ -67,10 +121,24 @@ void MainWindow::on_actionHelp_triggered()
 
 void MainWindow::on_actionVersion_triggered()
 {
-    sendCommand("version");
+    sendCommand("version", [](QStringList & x) {
+        qDebug() << x;
+    });
 }
 
 void MainWindow::on_actionShowPort_triggered()
 {
-    sendCommand("showport");
+    sendCommand("showport", [](QStringList & x) {
+        qDebug() << x;
+    });
+}
+
+void MainWindow::on_actionClear_triggered()
+{
+    ui->mainText->clear();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -8,34 +8,50 @@ namespace Ui {
 class MainWindow;
 }
 
+typedef void (*CALLBACK)(QStringList & data);
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+    typedef enum {
+        STATE_INIT,
+        STATE_IDLE,
+        STATE_INCMD,
+    } STATE;
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
 
-    void    sendCommand(const QString & cmd);
+    void    sendCommand(const QString & cmd, CALLBACK cb = nullptr);
 
 signals:
 
 protected slots:
     void newData(const char *buff, int len);
+    void stateChanged(QAbstractSocket::SocketState state);
 
 protected:
     QTelnet                         telnet;
 
-//    void                            showEvent(QShowEvent *);
 private slots:
     void on_sendButton_clicked();
     void on_actionHelp_triggered();
     void on_actionVersion_triggered();
     void on_actionShowPort_triggered();
 
+    void on_actionClear_triggered();
+
+    void on_actionSave_triggered();
+
 private:
     Ui::MainWindow *                ui                  = nullptr;
+    bool                            bCmdInProgress      = false;
     QString                         command;
     QString                         response;
+    CALLBACK                        _cb                 = nullptr;
+
+    STATE                           state               = STATE_INIT;
 };
 
 #endif // MAINWINDOW_H
